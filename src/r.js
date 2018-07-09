bfe924d1ebbd10e127bb4019856abe3a0cb9040b
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-07-09 12:25:42 +0800

    ok

--- src/r.js
+++ src/r.js
@@ -6,25 +6,12 @@
 
   Object.assign(self,{R,render,fc});
 
-  function fc( t, frag = false ) {
-    const fragment = parser.parseFromString(
-      `<template>${t}</template>`,
-      "text/html")
-      .head
-      .firstElementChild
-      .content;
-    if ( frag ) { 
-      return fragment;
-    }
-    return fragment.firstElementChild;
-  }
-
   function R(parts, ...vals) {
     parts = Array.from(parts);
     const handlers = {};
     vals = vals.map( v => {
       if ( Array.isArray(v) && v.every(item => !!item.handlers && !!item.str) ) {
-        return join(v,handlers);
+        return join(v) || '';
       } else if ( typeof v == "object" && !!v && !(v.str && v.handlers) ) {
         throw { error: `Object properties not allowed`, value: v };
       } else return (v == null || v == undefined) ? '' : v;
@@ -45,8 +32,7 @@
       if ( typeof val == "function" ) {
         let attrName;
         if ( attrNameMatches && attrNameMatches.length > 1) {
-          attrName = attrNameMatches[1]; 
-          attrName = attrName.replace(/^on/,'').toLowerCase();
+          attrName = attrNameMatches[1].replace(/^on/,'').toLowerCase();
         }
         const newPart = part.replace(attrNameMatches[0], '');
         if ( attrName ) {
@@ -65,7 +51,7 @@
       } else if ( !! val && val.handlers && val.str ) {
         Object.assign(handlers,val.handlers);
         str += part;
-        val = val.str; 
+        val = val.str;
         if ( attrNameMatches ) {
           val = `"${val}"`;
         }
@@ -73,7 +59,9 @@
       } else {
         str += part;
         if ( attrNameMatches ) {
-          val = `"${val}"`;
+          val = `"${safe(val)}"`;
+        } else {
+          val = safe(val);
         }
         str += val;
       }
@@ -82,20 +70,12 @@
     return {str,handlers};
   }
 
-  function join(rs,allHandlers) {
-    return rs
-      .map(
-        ({str,handlers}) => (Object.assign(allHandlers,handlers),str)
-      ).join('\n');
-  }
-
   function render(r, root, {replace:replace = false} = {}) {
-    if ( Array.isArray(r) && r.every( val => !!val.str && !!val.handlers ) ) {
-      r = join(r,handlers); 
+    if (Array.isArray(r) && r.every(val => !!val.str && !!val.handlers)) {
+      r = join(r);
     }
     let {str,handlers} = r;
     if ( replace ) {
-      const c = fc(str);
       root.parentElement.replaceChild(fc(str),root);
     } else {
       root.innerHTML = '';
@@ -112,5 +92,25 @@
         throw {error: `Node or handlers could not be found for ${hid}`, hid};
       }
     });
+  }
+  
+  function join(rs) {
+    const H = {};
+    const str = rs.map(({str,handlers}) => (Object.assign(H,handlers),str)).join('\n');
+    if ( !! str ) {
+      return {str,handlers:H};
+    }
+  }
+  
+  function safe(v) {
+    return (v+'').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/&/g,'&amp;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
+  }
+
+  function fc(t, frag = false) {
+    const fragment = parser.parseFromString(`<template>${t}</template>`,"text/html").head.firstElementChild.content;
+    if ( frag ) {
+      return fragment;
+    }
+    return fragment.firstElementChild;
   }
 }
